7a5d8e55cbd0d04c9082e02c6559152d366ae597
LAPTOP-VR6LS9FC\jp21w <jp21.wayne@gmail.com>
2023-03-09 09:46:32 +0800

    增加更新欄位的seeder

--- database/seeders/RefreshFieldSeeder.php
+++ database/seeders/RefreshFieldSeeder.php
@@ -2,33 +2,22 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class RefreshFieldSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-
         DB::statement('SET FOREIGN_KEY_CHECKS=0;');
         
         $this->call(MenuSeeder::class);
         $this->call(MenuDetailSeeder::class);
-        $this->call(GroupSeeder::class);
-        $this->call(RoleSeeder::class);
-        $this->call(UserSeeder::class);
         $this->call(PermissionSeeder::class);
         $this->call(GroupPermissionSeeder::class);
         $this->call(RolePermissionSeeder::class);
