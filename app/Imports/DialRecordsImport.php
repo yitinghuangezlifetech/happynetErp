b55ce0f828972070f5302876d53b5a3a1e777938
LAPTOP-VR6LS9FC\jp21w <jp21.wayne@gmail.com>
2023-08-05 09:58:47 +0800

    debug

--- app/Imports/DialRecordsImport.php
+++ app/Imports/DialRecordsImport.php
@@ -267,6 +267,99 @@ class DialRecordsImport implements ToCollection
                     }
                 }
                 break;
+            case 'FET IMS(080-0701全區單區-市話-長途-行動)':
+                foreach ($rows as $k=>$row)
+                {
+                    if ($k > 0)
+                    {
+                        $user = app(User::class)->where('telecom_number', $this->removeBlankSpace(str_replace("'", "", $row[0])))->first();
+
+                        $recordDay = explode('.', $this->removeBlankSpace($row[2]));
+                        $startTime = $this->removeBlankSpace(str_replace("'", "", $row[8]));
+                        $tmp = explode(':', $this->removeBlankSpace(str_replace("'", "", $row[9])));
+                        $addHours = $tmp[0];
+                        $addMinutes = $tmp[1];
+                        $addSeconds = $tmp[2];
+
+                        $carbonObj = Carbon::createFromFormat('H:i:s', $startTime);
+                        $carbonObj->addHours($addHours);
+                        $carbonObj->addMinutes($addMinutes);
+                        $carbonObj->addSeconds($addSeconds);
+
+                        $endTime = $carbonObj->format('H:i:s');
+
+                        $carbonObj = Carbon::createFromFormat('H:i:s', $this->removeBlankSpace(str_replace("'", "", $row[9])));
+                        $seconds = $carbonObj->diffInSeconds(Carbon::createFromTime(0, 0, 0));
+
+                        app(DialRecord::class)->create([
+                            'id' => uniqid(),
+                            'batch_no' => $batchNo,
+                            'dail_record_type_id' => $this->type->id,
+                            'user_id' => $user->id??NULL,
+                            'telecom_account' => $this->removeBlankSpace(str_replace("'", "", $row[0])),
+                            'tel_number' => $this->removeBlankSpace(str_replace("'", "", $row[1])),
+                            'record_day' => $this->removeBlankSpace($row[2]),
+                            'call_type' => $this->removeBlankSpace($row[3]),
+                            'dial_location' => $this->removeBlankSpace($row[4]),
+                            'dial_number' => $this->removeBlankSpace(str_replace("'", "", $row[5])),
+                            'accept_location' => $this->removeBlankSpace($row[6]),
+                            'accept_number' => $this->removeBlankSpace(str_replace("'", "", $row[7])),
+                            'record_day_ad' => $this->convertToAD($recordDay[0]).'-'.sprintf('%02d', $recordDay[1]).'-'.sprintf('%02d', $recordDay[2]),
+                            'start_time' => $startTime,
+                            'end_time' => $endTime,
+                            'talking_time' => $this->removeBlankSpace(str_replace("'", "", $row[9])),
+                            'sec' => $seconds,
+                            'period' => $this->removeBlankSpace($row[10]),
+                            'fee' => $this->removeBlankSpace($row[11]),
+                            'note' => $this->removeBlankSpace($row[12]),
+                        ]);
+                    }
+                }
+                break;
+            case 'Jarrow 通聯記錄':
+                //資料欄位格式
+                //系統商,用戶代號,附掛號碼,來源IP,目的端號碼,目的端IP,開始時間,結束時間,前置碼,通話秒數,費用
+
+                foreach ($rows as $k=>$row)
+                {
+                    if ($k > 0)
+                    {
+                        $user = app(User::class)->where('telecom_number', $this->removeBlankSpace(str_replace("'", "", $row[1])))->first();
+
+                        $start = explode(' ', $row[6]);
+                        $end = explode(' ', $row[7]);
+
+                        $start_1 = Carbon::createFromFormat('H:i:s', $start[1]);
+                        $end_1 = Carbon::createFromFormat('H:i:s', $end[1]);
+
+                        $durationInSeconds = $end_1->diffInSeconds($start_1);
+                        $formattedDuration = gmdate('H:i:s', $durationInSeconds);
+
+                        $recordDay = $start[1];
+
+                        app(DialRecord::class)->create([
+                            'id' => uniqid(),
+                            'batch_no' => $batchNo,
+                            'dail_record_type_id' => $this->type->id,
+                            'company_code' => $this->removeBlankSpace($row[0]),
+                            'user_id' => $user->id??NULL,
+                            'telecom_account' => $this->removeBlankSpace(str_replace("'", "", $row[1])),
+                            'tel_number' => $this->removeBlankSpace(str_replace("'", "", $row[2])),
+                            'record_day' => $recordDay,
+                            'source_ip' => $this->removeBlankSpace($row[3]),
+                            'accept_number' => $this->removeBlankSpace(str_replace("'", "", $row[4])),
+                            'accept_IP' => $this->removeBlankSpace(str_replace("'", "", $row[5])),
+                            'record_day_ad' => $recordDay,
+                            'start_time' => $start[1],
+                            'end_time' => $end[1],
+                            'talking_time' => $formattedDuration,
+                            'sec' => $durationInSeconds,
+                            'frontent_code' => $this->removeBlankSpace($row[8]),
+                            'fee' => $this->removeBlankSpace($row[10]),
+                        ]);
+                    }
+                }
+                break;
 
         }
     }
